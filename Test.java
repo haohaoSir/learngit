50f4b27e463b2ef5af28f23b9eb208ae512f5726
haohao <haohao_zyt@foxmail.com>
2016-03-23 13:45:59 +0800

    添加一行输出‘hello zhuhan’

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
 	public static void main(String args[]){
 		System.out.println("herllo world");
 		System.out.println("hello haohao");
+		System.out.println("hello zhuhan");
 	}
 
 }
